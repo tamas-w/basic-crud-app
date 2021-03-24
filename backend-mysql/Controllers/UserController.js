4957455ddc7c5bc4e9081ca988f987ddb42ae686
Tamas Wittek <tammas19@gmail.com>
2021-03-24 18:55:25 +0100

    feat(backend-mysql): created UserController file and added register method

---
+++ backend-mysql/Controllers/UserController.js
@@ -0,0 +1,18 @@
+import { UserService } from '../Services/UserService.js';
+
+export class UserController {
+  constructor() {
+    this.userService = new UserService();
+    this.register = this.register.bind(this);
+  }
+
+  async register(req, res) {
+    try {
+      const { username, password, email } = req.body;
+      const data = await this.userService.register(username, password, email);
+      res.status(200).json(data);
+    } catch (err) {
+      res.status(400).json(err.message);
+    }
+  }
+}
