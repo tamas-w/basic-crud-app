a3cf8932413253a5b60fad56e943525a62b83bd9
Tamas Wittek <tammas19@gmail.com>
2021-03-24 18:15:30 +0100

    feat(backend-mysql): Created SessionService with login method

--- backend-mysql/Controllers/SessionController.js
+++ backend-mysql/Controllers/SessionController.js
@@ -2,14 +2,13 @@ import { SessionService } from '../Services/SessionService.js';
 
 export class SessionController {
   constructor() {
-    this.login = this.login.bind(this);
     this.sessionService = new SessionService();
+    this.login = this.login.bind(this);
   }
   async login(req, res) {
     try {
       const { password, email } = req.body;
       const data = await this.sessionService.login(password, email);
-      req.session.isAuth = true;
       res.status(200).json(data);
     } catch (err) {
       res.status(400).json(err.message);
