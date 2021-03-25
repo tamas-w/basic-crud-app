5a75b2ed605753205023042c33451a9b230bbcda
Tamas Wittek <tammas19@gmail.com>
2021-03-25 13:56:43 +0100

    feat(backend-mysql): created user registration input validator and added to userRoutes

--- backend-mysql/Routes/UserRoutes.js
+++ backend-mysql/Routes/UserRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { UserController } from '../Controllers/UserController.js';
+import { validateUser } from '../Middlewares/UserRegistrationValidator.js';
 
 const router = express.Router();
 
 const userController = new UserController();
 
-router.post('/', (req, res) => {
+router.post('/', validateUser,(req, res) => {
   userController.register(req, res);
 });
 
