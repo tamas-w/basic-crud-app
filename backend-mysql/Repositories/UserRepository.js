740c6ad65ea176b1753bf0ad457b8a3ba479281e
Tamas Wittek <tammas19@gmail.com>
2021-03-25 14:16:34 +0100

    fix(backend-mysql): updated table name in queries

--- backend-mysql/Repositories/UserRepository.js
+++ backend-mysql/Repositories/UserRepository.js
@@ -2,15 +2,14 @@ import { db } from '../data/connection.js';
 
 export class UserRepository {
   async findUser(email) {
-    const [rows] = await db.query('SELECT * FROM user where email = ?', [
+    const [rows] = await db.query('SELECT * FROM users where email = ?', [
       email,
     ]);
     return rows;
   }
   async registerUser(username, password, email) {
-    console.log(' name, pw, email', username, password, email);
-    const [rows] = await db.query(
-      'INSERT INTO user (username, password, email) VALUES (?, ?, ?)',
+    const rows = await db.query(
+      'INSERT INTO users (name, password, email) VALUES (?, ?, ?)',
       [username, password, email]
     );
     return rows;
