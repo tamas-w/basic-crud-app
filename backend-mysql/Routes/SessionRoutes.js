daa72ff73bf89c14821e47bacaec0bb826fc2435
Tamas Wittek <tammas19@gmail.com>
2021-04-14 22:14:09 +0200

    fix: added missing import, and created SessionController class instance

--- backend-mysql/Routes/SessionRoutes.js
+++ backend-mysql/Routes/SessionRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { SessionController } from '../Controllers/SessionController.js';
 
 const router = express.Router();
+const sessionController = new SessionController();
 
 router.post('/', (req, res) => {
   sessionController.login(req, res);
